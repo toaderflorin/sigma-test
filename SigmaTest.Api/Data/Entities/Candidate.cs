6b3b6c57f207b8263bc390bf0a03c70b5dbea61f
Florin Toader <florin@papertrail.io>
2024-07-24 18:06:29 +0300

    Initial commit.

---
+++ SigmaTest.Api/Data/Entities/Candidate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SigmaTest.Api.Controllers.Entities
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTimeOffset TimeIntervalStart { get; set; }
+
+        public DateTimeOffset TimeInterValEnd { get; set; }
+
+        public string LinkedInUrl { get; set; } = string.Empty;
+    }
+}
+
---
+++ SigmaTest.Api/Logic/CandidateService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SigmaTest.Api.Controllers.Entities;
+
+namespace SigmaTest.Api.Logic
+{
+	public class CandidateService
+	{
+		public CandidateService()
+		{
+		}
+
+		public void Add(Candidate candidate)
+		{
+
+		}
+
+		public void Update(Candidate candidate)
+		{
+
+		}
+
+		public void RemoveById(int id)
+		{
+
+		}
+	}
+}
+
